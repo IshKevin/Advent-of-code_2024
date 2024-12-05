15d72ad5a5e9fd69bae66ef1672a430392dbbe50
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-12-05 09:51:56 +0200

    Add implementation for Day 4 part 1 solution to count occurrences of 'XMAS' in a grid

---
+++ day4/part1.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+
+var directions = [8][2]int{
+    {0, 1},   
+    {0, -1},  // left
+    {1, 0},   // down
+    {-1, 0},  // up
+    {1, 1},   // down-right
+    {1, -1},  // down-left
+    {-1, 1},  // up-right
+    {-1, -1}, // up-left
+}
+
+func isValid(x, y, rows, cols int) bool {
+    return x >= 0 && x < rows && y >= 0 && y < cols
+}
+
+func hasXMAS(grid []string, x, y int, direction [2]int) bool {
+    word := "XMAS"
+    wordLen := len(word)
+    rows := len(grid)
+    cols := len(grid[0])
+
+    for k := 0; k < wordLen; k++ {
+        newX := x + k*direction[0]
+        newY := y + k*direction[1]
+        if !isValid(newX, newY, rows, cols) || grid[newX][newY] != word[k] {
+            return false
+        }
+    }
+    return true
+}
+
+func countOccurrences(grid []string, word string) int {
+    count := 0
+    rows := len(grid)
+    cols := len(grid[0])
+
+    for i := 0; i < rows; i++ {
+        for j := 0; j < cols; j++ {
+            for _, direction := range directions {
+                if hasXMAS(grid, i, j, direction) {
+                    count++
+                }
+            }
+        }
+    }
+    return count
+}
+
+func readGridFromFile(filename string) ([]string, error) {
+    file, err := os.Open(filename)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    var grid []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        grid = append(grid, scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        return nil, err
+    }
+
+    return grid, nil
+}
+
+func main() {
+    grid, err := readGridFromFile("input.txt")
+    if err != nil {
+        fmt.Println("Error reading file:", err)
+        return
+    }
+
+    word := "XMAS"
+    result := countOccurrences(grid, word)
+    fmt.Println("Total occurrences of", word, ":", result)
+}
