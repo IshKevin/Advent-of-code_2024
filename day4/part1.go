743016dbdd868a1352a0ab06f38556924aae02e7
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-12-05 10:06:06 +0200

    Refactor direction array formatting for improved readability

--- day4/part1.go
+++ day4/part1.go
@@ -9,13 +9,13 @@ import (
 
 var directions = [8][2]int{
     {0, 1},   
-    {0, -1},  // left
-    {1, 0},   // down
-    {-1, 0},  // up
-    {1, 1},   // down-right
-    {1, -1},  // down-left
-    {-1, 1},  // up-right
-    {-1, -1}, // up-left
+    {0, -1},  
+    {1, 0},   
+    {-1, 0}, 
+    {1, 1},   
+    {1, -1},
+    {-1, 1},  
+    {-1, -1}, 
 }
 
 func isValid(x, y, rows, cols int) bool {
